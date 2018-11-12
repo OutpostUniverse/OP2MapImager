f6aaa21efc1809c9a5a65fa1f732826f84a8b250
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-11-12 08:06:15 -0500

    Update FreeImageBmp constructors to throw if bitmap ill formed

--- src/FreeImageBmp.cpp
+++ src/FreeImageBmp.cpp
@@ -4,17 +4,31 @@
 //FreeImageBmp::FreeImageBmp(FIBITMAP* fiBitmap) : fiBitmap(fiBitmap) {}
 
 FreeImageBmp::FreeImageBmp(int width, int height, int bpp) : 
-	fiBitmap(FreeImage_Allocate(width, height, bpp)) {}
+	fiBitmap(FreeImage_Allocate(width, height, bpp)) 
+{
+	if (fiBitmap == nullptr) {
+		throw std::runtime_error("Unable to create a default bitmap with the following properties, Width: " + std::to_string(width) +
+			" , Height: " + std::to_string(height) + " , Bits per pixel: " + std::to_string(bpp));
+	}
+}
 
 FreeImageBmp::FreeImageBmp(FREE_IMAGE_FORMAT imageType, FIMEMORY* fiMemory) : 
-	fiBitmap(FreeImage_LoadFromMemory(imageType, fiMemory, 0)) {}
+	fiBitmap(FreeImage_LoadFromMemory(imageType, fiMemory, 0)) 
+{
+	if (fiBitmap == nullptr) {
+		throw std::runtime_error("Unable to load a bitmap from provided location in memory");
+	}
+}
 
 FreeImageBmp::FreeImageBmp(FREE_IMAGE_FORMAT imageFormat, const std::string& filename) :
-	fiBitmap(FreeImage_Load(imageFormat, filename.c_str())) {}
+	fiBitmap(FreeImage_Load(imageFormat, filename.c_str())) 
+{
+	if (fiBitmap == nullptr) {
+		throw std::runtime_error("Unable to load bitmap located at " + filename);
+	}
+}
 
 FreeImageBmp::~FreeImageBmp()
 {
-	if (fiBitmap != nullptr) {
-		FreeImage_Unload(fiBitmap);
-	}
+	FreeImage_Unload(fiBitmap);
 }
